8c46398b481133ebb2f845df653c29f4b196302a
k3b <1374583+k3b@users.noreply.github.com>
2018-10-31 15:46:16 +0100

    Merge fix

--- libK3bZip/src/main/java/de/k3b/zip/CompressJob.java
+++ libK3bZip/src/main/java/de/k3b/zip/CompressJob.java
@@ -40,7 +40,6 @@ import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 import de.k3b.io.StringUtils;
-import de.k3b.io.ZipStorage;
 
 /**
  * Android independant enginge that adds files to a zip file via a compressQue.
